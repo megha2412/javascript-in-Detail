57a151cd35931ac10859b8c9cb8ea8b6e540cee4
meghakamble24 <megha@osplabs.com>
2025-05-03 21:29:07 +0530

    new changes

---
+++ js-basics/basics.js
@@ -0,0 +1,7 @@
+// three types of variables
+// const
+// let 
+// var --dont use as dont understand engine the scope of this variables
+
+"use strict"; //no use of the old javascript
+ alert("hello"); //we are using nodejs not browser so cant use this functionality
---
+++ js-basics/datatypes.js
@@ -0,0 +1,12 @@
+
+
+let data=undefined;
+console.log(typeof(data));
+function abc(){
+    console.log('hi');
+    
+}
+
+console.log(typeof abc); // it will return the function
+
+// so except the null all return their respective datatype means datatypes
